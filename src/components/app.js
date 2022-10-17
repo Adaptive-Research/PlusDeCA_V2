c4ccfa0e2f7bc57ca153d6f4a7bef2dd8f0466a2
Pericles001 <periclesadjovi@gmail.com>
2022-10-17 16:24:47 +0100

    Change conditional rendering

--- src/components/app.js
+++ src/components/app.js
@@ -10,62 +10,49 @@ import TabToTop from "../layouts/TabToTop/TabToTop";
 import Login from "../components/CustomPages/Login/Login" ;
 
 
-//import Errorpage401 from "./ErrorPages/ErrorPages/401/401";
-
 export default function App() {
 
-  //console.log(process.env) ;
+  console.log(localStorage.getItem("logged"));
 
 
-  const lastLogin =  localStorage.getItem('lastLogin') ;
-  if (!lastLogin)
-    return <Login /> ;
-
-
-  let LastLogin = new Date(lastLogin) ;
-  const Now = new Date() ;
-
-  var Difference_In_Time = Now.getTime() - LastLogin.getTime();
-  var Difference_In_Days = Difference_In_Time / (1000 * 3600 * 24);
-  if (Difference_In_Days >= 1)
-  {
-    console.log("Difference_In_Days") ;
-    console.log(Difference_In_Days) ;
-    return <Login /> ;
-  }
-  else
-  {
+  if (localStorage.getItem("logged") === "true"){
     return (
-      <Fragment>
-        <div className="horizontalMenucontainer">
-          <TabToTop />
-          <div className="page">
-            <div className="page-main">
-              <Header />
-              <Sidebar />
+        <Fragment>
+          <div className="horizontalMenucontainer">
+            <TabToTop />
+            <div className="page">
+              <div className="page-main">
+                <Header />
+                <Sidebar />
 
-              <div className="main-content app-content ">
-                <div className="side-app">
-                  <div
-                    className="main-container container-fluid"
-                    onClick={() => {
-                      Switcherdata.responsiveSidebarclose();
-                      Switcherdata.Horizontalmenudefultclose();
-                    }}
-                  >
-                    <Outlet />
+                <div className="main-content app-content ">
+                  <div className="side-app">
+                    <div
+                        className="main-container container-fluid"
+                        onClick={() => {
+                          Switcherdata.responsiveSidebarclose();
+                          Switcherdata.Horizontalmenudefultclose();
+                        }}
+                    >
+                      <Outlet />
+                    </div>
                   </div>
                 </div>
+
+
               </div>
-
-
+              <RightSidebar />
+              <Switcher />
+              <Footer />
             </div>
-            <RightSidebar />
-            <Switcher />
-            <Footer />
           </div>
-        </div>
-      </Fragment>
+        </Fragment>
+    );
+  } else {
+    return (
+        <Login />
     );
   }
+
+
 }
