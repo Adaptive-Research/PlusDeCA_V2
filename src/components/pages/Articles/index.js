3a08f78d890e00d3f4ef810c2aeb95f43d14ae62
Pericles001 <periclesadjovi@gmail.com>
2022-10-24 13:55:37 +0100

    Create files for article management

---
+++ src/components/pages/Articles/index.js
@@ -0,0 +1,5 @@
+import ArticlesList from "./List/ArticleList";
+import ArticleAdd from "./Add/ArticleAdd";
+import ArticleEdit from "./Edit/ArticleEdit";
+
+export {ArticlesList, ArticleAdd, ArticleEdit}
