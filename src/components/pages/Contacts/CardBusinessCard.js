8072378536165d2de2cfffe01cda732f6ce9a7d1
MHTech229 <109033815+MHTech229@users.noreply.github.com>
2022-11-24 16:46:11 +0100

    0.51

--- src/components/pages/Contacts/CardBusinessCard.js
+++ src/components/pages/Contacts/CardBusinessCard.js
@@ -30,9 +30,11 @@ export default function CardBusinessCard(props) {
                 <div className='BusinessCard-Content'>
                     <h4 className='Prospect-FullName'>{props.BusinessCard.Prenom} {props.BusinessCard.Nom}</h4>
                     <p className='Prospect-Fonction'>{props.BusinessCard.Fonction}</p>
-                    <p className='Prospect-Email'><IoMdMail /> {props.BusinessCard.Email}</p>
-                    <p className='Prospect-Telephone'><IoMdPhonePortrait /> {props.BusinessCard.Telephone}</p>
-                    <p className='Prospect-SiteWeb'><FaGlobe /> {props.BusinessCard.SiteWeb}</p>
+                    <div className='BusinessCard-Coordonees'>
+                        <p className='Prospect-Email'><IoMdMail /> {props.BusinessCard.Email}</p>
+                        <p className='Prospect-Telephone'><IoMdPhonePortrait /> {props.BusinessCard.Telephone}</p>
+                        <p className='Prospect-SiteWeb'><FaGlobe /> {props.BusinessCard.SiteWeb}</p>
+                    </div>
                 </div>                  
                 <div className='BusinessCardBtns'>
                     <button className='btn btn-primary' onClick={() => EditBusinessCard(props.BusinessCard)}>
