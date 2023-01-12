784d26c5d774066a04fa251c8cb2b9ed1969f8fe
Adaptive-Research <ddupard@adaptive-research.eu>
2023-01-12 09:44:59 +0100

    0.78

--- src/components/pages/Articles/ArticleList.js
+++ src/components/pages/Articles/ArticleList.js
@@ -1,4 +1,4 @@
-import React, { useState,useRef,useEffect} from "react";
+import React, { useState,useRef} from "react";
 import {Card, Col, Row, Tab, Tabs} from "react-bootstrap";
 import {FindTranslation,getIDFromToken} from  "../../../data/customlibs/utils" ;
 import {getUserArticles} from "../../../data/customlibs/api";
